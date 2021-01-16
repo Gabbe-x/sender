6a43603da6806f1f0f5505026a001583c331c355
Gabbe-x <72690663+Gabbe-x@users.noreply.github.com>
2021-01-17 01:32:06 +0700

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -10,10 +10,10 @@ COUNT=15
 while [ $COUNT -gt 0 ]
 do
 coda client send-payment \
--sender $SENDER
--reciver $RECIVER
--fee $FEE
--amount $AMOUNT
+-sender B62qrmZqGMetG6JZj8dFnoagGhX298CjZ42Jdc8YKGTmvi4YHUYBb1p \
+-receiver B62qjUn8PNtvKHMg7wAwD3o2CBNHE7gm4ZX2H8A8jqkbuhKnGioCM7T \
+-fee 0.1 \
+-amount 2
 sleep 15
 ((COUNT--))
 done
