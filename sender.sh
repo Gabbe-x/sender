e5eed0dcd9172c223e7596d522a801175da65be3
Gabbe-x <72690663+Gabbe-x@users.noreply.github.com>
2021-01-17 01:00:58 +0700

    Add files via upload

---
+++ sender.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SENDER=B62qpjbpDtVVa6DJWFFz2Xi5PSQ1KR7cX1LzoKcii7bLo1mZZt18APV
+RECIVER=B62qjUn8PNtvKHMg7wAwD3o2CBNHE7gm4ZX2H8A8jqkbuhKnGioCM7T
+FEE=210
+AMOUNT=0.1
+
+COUNT=15
+
+while [ $COUNT -gt 0 ]
+do
+SHELLCOMMAND=$(echo -e "code client send-payment -sender $SENDER -reciver $RECIVER -fee $FEE -amount $AMOUNT")
+$SHELCOMMAND
+sleep 15
+((COUNT--))
+done
